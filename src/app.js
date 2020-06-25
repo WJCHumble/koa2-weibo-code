24894a20fd2d1a7b692bd4b68b6b4909caa63eb3
Apple <1957503379@qq.com>
2020-06-26 01:09:50 +0800

    refactor: 调整目录结构

--- src/app.js
+++ src/app.js
@@ -14,10 +14,12 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
+// 控制台的日志打印
 app.use(logger())
+// koa-static 即将 public 目录作为静态资源直接访问
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
@@ -25,12 +27,12 @@ app.use(views(__dirname + '/views', {
 }))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
