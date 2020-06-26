a7c5e3fee5e769dde141e90196d8f130debcdc5d
Apple <1957503379@qq.com>
2020-06-26 11:59:12 +0800

    feat: 路由演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,37 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "Hello Koa 2!",
+  });
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+router.get("/string", async (ctx, next) => {
+  ctx.body = "koa2 string";
+});
 
-router.get('/json', async (ctx, next) => {
+router.get("/json", async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    title: "koa2 json",
+  };
+});
 
-module.exports = router
+router.get("/profile/:userName", async (ctx, next) => {
+  const { userName } = ctx.params;
+  ctx.body = {
+    title: "this is profile page",
+    userName,
+  };
+});
+
+// 一个获取 JSON 类型数据的 Post 接口
+router.post("/login", async (ctx, next) => {
+  const { userName, password } = ctx.request.body;
+  ctx.body = {
+    tag: 100,
+    userName,
+    password,
+  };
+});
+
+module.exports = router;
